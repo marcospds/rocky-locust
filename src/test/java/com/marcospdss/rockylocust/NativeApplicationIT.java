740103b08a7969156da6c1632f9059b5cb7f5517
Marcos Paulo dos Santos <marcos.paulo@totvs.com.br>
2019-09-18 00:23:20 -0300

    First version with Quarkus

---
+++ src/test/java/com/marcospdss/rockylocust/NativeApplicationIT.java
@@ -0,0 +1,9 @@
+package com.marcospdss.rockylocust;
+
+import io.quarkus.test.junit.SubstrateTest;
+
+@SubstrateTest
+public class NativeApplicationIT extends ApplicationTest {
+
+    // Execute the same tests but in native mode.
+}
